b7f66775a2796c2a054ff536321d527d2c7112b5
BradTheeStallion <156148735+BradTheeStallion@users.noreply.github.com>
2024-03-15 11:04:36 -0230

    Update CoolStuff.py
    fixes

--- CoolStuff.py
+++ CoolStuff.py
@@ -73,10 +73,10 @@ def Hst(PriceInput):
     #The values are returned as a tuple, an object that can be indexed like a list.
     return HstAmt, TotalAmt, PriceInputDSP, HstAmtDsp, TotalAmtDsp
 
-def AnyKey(prompt):
+def AnyKey(Prompt):
     #The following function advances the program after any key is pressed. First, end='' and flush=True are used to make the print function behave (keeping the cursor at the end of that line) Next, the code checks the operating system and then proceeds accordingly. In windows, the msvcrt.getch() function records the keystroke as a byte string, but doesn't produce anything in the console. In Unix systems (such as Linux or MacOS), the terminal is set to raw mode in the try statement,temporarilly (for one keystroke) preventing the character from appearing on the console. The finally statement brings the terminal back out of raw mode. This isn't the extra feature lol. I chose to import the modules within the function because it crashed the code for me otherwise.
     import sys
-    print(prompt, end='', flush=True)
+    print(Prompt, end='', flush=True)
     if sys.platform.startswith('win'):
         import msvcrt
         msvcrt.getch()
@@ -91,13 +91,13 @@ def AnyKey(prompt):
         finally:
             termios.tcsetattr(FileDesc, termios.TCSADRAIN, OldSettings)
             
-def Whoops(prompt):
+def Whoops(Prompt):
     #This function is a little escape hatch to allow the users to get out of a program without running it. Example use:
     # Main program
     #while True:
         #if not Whoops("If you accidentally chose Option 1,\ntype END and press return to go back to the main menu.\nOtherwise, press return to continue: "):
             #break
-    print(prompt, end='', flush=True)
+    print(Prompt, end='', flush=True)
     Oops = input().upper()
     print()
     if Oops == "END":
@@ -118,23 +118,24 @@ def NotBlank(UserInput):
             return False
         else:
             return UserInput
-
-def ValidNameAdd(prompt):
-    #Makes sure field isn't blank and formats names and cities to my preference
+        
+def ValidNameAdd(Prompt):
+    #Makes sure field isn't blank and formats cities to my preference
     import string
     while True:
-        UserInput = NotBlank(input(prompt))
+        UserInput = NotBlank(input(Prompt))
         if not UserInput:
             continue
         else:
             return string.capwords(UserInput), UserInput.title()
-
-def MoneyFloat(prompt):
+            
+            
+def MoneyFloat(Prompt):
     #This function uses regex to ensure that a number is not only a float, but has no more than the two decimal places expected for a monetary value. There were fewer monetary inputs than expected, but we're proud of this and it will definitely be recycled in future code.
     import re
     pattern = r'^\d+(\.\d{1,2})?$'
     while True:
-        UserFloat = NotBlank(input(prompt))
+        UserFloat = NotBlank(input(Prompt))
         if not UserFloat:
             continue
         elif not re.match(pattern, UserFloat):
@@ -142,10 +143,10 @@ def MoneyFloat(prompt):
         else:
             return float(UserFloat), "${:,.2f}".format(float(UserFloat))
         
-def ValidPhone(prompt):
+def ValidPhone(Prompt):
     #This functions validates phone numbers to suit my preferred input format.
     while True:
-        PhoneNum = NotBlank(input(prompt))
+        PhoneNum = NotBlank(input(Prompt))
         if not PhoneNum:
             continue
         elif len(PhoneNum) != 10:
@@ -155,43 +156,45 @@ def ValidPhone(prompt):
         else:
             return PhoneNum, f"({PhoneNum[0:3]}) {PhoneNum[3:6]}-{PhoneNum[6:]}"
 
-def ValidPost(prompt):
+def ValidPost(Prompt):
     #This functions validates postal codes to suit my preferred input format.
     while True:
-        PostCode = NotBlank(input(prompt).upper())
+        PostCode = NotBlank(input(Prompt))
         if not PostCode:
             continue
         elif len(PostCode) != 6 or not PostCode[0].isalpha() or not PostCode[2].isalpha() or not PostCode[4].isalpha() or not PostCode[1].isdigit() or not PostCode[3].isdigit() or not PostCode[5].isdigit():
             Padding("Error: Invalid postal code.")
         else:
-            return PostCode, f"{PostCode[0:3]} {PostCode[3:]}"
+            return PostCode.upper(), f"{PostCode[0:3]} {PostCode[3:]}".upper()
     
-def ValidProv(prompt):
+def ValidProv(Prompt):
     #This functions validates province abbreviations to suit my preferred input format.
     while True:
         ProvList = ["NL", "PE", "NS", "NB", "QC", "ON", "MB", "SK", "AB", "BC", "YT", "NT", "NU"]
-        Province = NotBlank(input(prompt).upper())
+        Province = NotBlank(input(Prompt))
         if not Province:
             continue
-        elif len(Province) != 2:
-            Padding("Error: Province must be two characters (XX).")
-        elif not Province.isalpha():
-            Padding("Error: Province must be two letters (XX).")
-        elif Province == "PQ":
-            Province = "QC"
-            return Province
-        elif Province == "NF":
-            Province = "NL"
-            return Province
-        elif Province not in ProvList:
-            Padding(f"Error: Value not recognized as Canadian Province.\n{ProvList}")
         else:
-            return Province
+            Province = Province.upper()
+            if len(Province) != 2:
+                Padding("Error: Province must be two characters (XX).")
+            elif not Province.isalpha():
+                Padding("Error: Province must be two letters (XX).")
+            elif Province == "PQ":
+                Province = "QC"
+                return Province
+            elif Province == "NF":
+                Province = "NL"
+                return Province
+            elif Province not in ProvList:
+                Padding(f"Error: Value not recognized as Canadian Province.\n{ProvList}")
+            else:
+                return Province
 
-def ValidPlate(prompt):
+def ValidPlate(Prompt):
     #This functions validates licence plates to suit my preferred input format.
     while True:
-        PlateNum = NotBlank(input(prompt).upper())
+        PlateNum = NotBlank(input(Prompt))
         if not PlateNum:
             continue
         elif len(PlateNum) != 6:
@@ -199,7 +202,7 @@ def ValidPlate(prompt):
         elif not PlateNum[0:3].isalpha() or not PlateNum[3:].isdigit():
             Padding("Error: Plate number must be three letters followed by three numbers.")
         else:
-            return PlateNum, f"{PlateNum[0:3]} {PlateNum[3:]}"
+            return PlateNum.upper(), f"{PlateNum[0:3]} {PlateNum[3:]}".upper()
 
 def AnimeScroll(Emoji = "(づ｡◕‿‿◕｡)づ", Phrase = "Mo is Tiggety-Boo!"):
     #I wanted to learn something new (the animation) while reviewing the append function for lists. The values defined are placeholders, another cool option would be calling AnimeScroll(input("Enter any ASCII emoji you want: "), input("Enter any phrase you want to appear with the emoji: ")) to allow user input.
@@ -219,12 +222,12 @@ def AnimeScroll(Emoji = "(づ｡◕‿‿◕｡)づ", Phrase = "Mo is Tiggety-Bo
         print("\r" + frame, end="")
         time.sleep(0.1)
         
-def ValidInt(prompt):
+def ValidInt(Prompt):
     #Use example:
     #MyNum = ValidInt("Number?: ")
     #print(MyNum)
     while True:
-        UserInput = NotBlank(input(prompt))
+        UserInput = NotBlank(input(Prompt))
         if not UserInput:
             continue
         else:
@@ -235,12 +238,12 @@ def ValidInt(prompt):
                 Padding("Error: Value must be a whole number.")
                 continue
 
-def ValidFloat(prompt):
+def ValidFloat(Prompt):
     #Use example:
     #MyFloat = ValidFloat("Number?: ")
     #print(MyFloat)
     while True:
-        UserInput = NotBlank(input(prompt))
+        UserInput = NotBlank(input(Prompt))
         if not UserInput:
             continue
         else:
@@ -250,7 +253,7 @@ def ValidFloat(prompt):
             except:
                 Padding("Error: Value must be a number.")
                 continue
-
+            
 def ValidYN(Prompt, Choice1 = 'Y', Choice2 = 'N'):
     #Makes sure user only enters Y or N (or specifications if different is required)
     while True:
